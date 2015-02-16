0a147080fb90b1888473ec39b8bf9897e406dfc9
Ravindra Bandara <rbandara@bn.com>
2015-02-16 15:17:45 -0500

    basic page working with jade

--- node/app.js
+++ node/app.js
@@ -1,15 +1,40 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  //send the index.html file for all requests
-  res.sendFile(__dirname + '/index.html');
+var express = require("express");
+var app = express();
+var port = 3000;
+
+//app.get("/", function(req, res){
+//    res.send("It works!");
+//});
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+
+//app.listen(port);
+var io = require('socket.io').listen(app.listen(port));
+
+console.log("Listening on port " + port);
+
+
+//var app = require('express')();
+//var http = require('http').Server(app);
+//var io = require('socket.io')(http);
+
+//app.get('/', function(req, res){
+//  send the index.html file for all requests
+//  res.sendFile(__dirname + '/index.html');
+//});
+
+//http.listen(3000, function(){
+//  console.log('listening on *:3000');
+//});
 
 // redis code
 var redis = require("redis");
